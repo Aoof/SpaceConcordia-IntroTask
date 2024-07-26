ef048dd6595cad644c411907a2ba8ce9ed7acc82
Abdulrahman Mousa <aoofxd@gmail.com>
2024-07-25 23:18:17 -0400

    Imported tutorials and init

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # SpaceConcordia-IntroTask
+
+### Tutorials
+- 
---
+++ ws_t1/build/turtlesim/turtlesim__py/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/aoof/Desktop/Coding/SpaceConcordia/ws_t1/build/turtlesim/turtlesim__py
+# Build directory: /home/aoof/Desktop/Coding/SpaceConcordia/ws_t1/build/turtlesim/turtlesim__py
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
